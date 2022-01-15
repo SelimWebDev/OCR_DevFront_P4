830a6e3ea132f85a0ee7f056bfe0f737ed8d018d
Selim El Mahi <selim.elmahi@gmail.com>
2022-01-15 15:47:47 +0100

    feat:ajout des test pour les input, et affiche d'un span error le cas échéant

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,11 +12,21 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelector(".close")
+const sumbitModalBtn = document.querySelector(".btn-submit")
+const errorSpan = document.getElementsByClassName("errorSpan")
+
+const firstName = document.getElementById("first")
+const lastName = document.getElementById("last")
+const email = document.getElementById("email")
+const birthdate = document.getElementById("birthdate")
+const competQuantity = document.getElementById("quantity")
+const locationCheckBox = document.querySelectorAll(".checkbox-location")
+const requiredCheckBox = document.getElementById("checkbox1")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// launch modal function
 function launchModal() {
   modalbg.style.display = "block";
 }
@@ -27,4 +37,85 @@ closeModalBtn.addEventListener("click", closeModal)
 // close modal function
 function closeModal(){
   modalbg.style.display = "none"
+}
+
+// function validate lancé au au submit
+function validate(event){
+
+  // status de validation des différents input
+  let status = {
+    firstNameStatus : false,
+    lastNameStatus : false,
+    emailStatus : false,
+    birthdateStatus : false,
+    competQuantStatus : false,
+    locationBoxStatus : false,
+    requiredBoxStatus : false
+  }
+  // les indices correspondent entre les 2 objets
+  // msg d'erreur
+  let errorMsg = {
+    firstNameErr : "Le prénom doit contenir au minimum 2 caractères",
+    lastNameErr : "Le nom doit contenir au minimum 2 caractères",
+    emailErr : "Veuillez saisir une adresse mail valide",
+    birthdateErr : "Saisir une data au format jj/mm/aaaa",
+    competQuantErr : "Seul les valeures numériques sont acceptés",
+    locationBoxErr : "Vous devez sélectionner une ville au minimum",
+    requiredBoxErr : "Vous devez accepter les conditions générales d'utilisation"
+  }
+  
+  //test prénom
+  if(firstName.value.length >= 2){        
+    status.firstNameStatus = true
+  }
+
+  //test nom
+  if(lastName.value.length >= 2){ 
+    status.lastNameStatus = true
+  }
+
+  //test mail
+  const mailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  if(mailRegExp.test(email.value)){
+    status.emailStatus = true
+  }
+
+  let birthdateTab = birthdate.value.split('-')    // on transforme brithdate en [aaaa, mm, dd]
+  if(birthdateTab[0].length == 4 && birthdateTab[1].length == 2 && birthdateTab[2].length == 2){   // on test le nombre d'élement
+    console.log("test longueur ok");
+    if(!isNaN(birthdateTab[0]) && !isNaN(birthdateTab[1]) && !isNaN(birthdateTab[2])){             // on test si c'est des chiffres
+      console.log("test nombre ok");
+      if(0<parseInt(birthdateTab[1]) &&  parseInt(birthdateTab[1])<13 && 0<parseInt(birthdateTab[2]) &&  parseInt(birthdateTab[2])<32){          // on test 1<=mm<=12 et 1<=jj=<31
+        console.log("test valeur du nombre ok");
+        status.birthdateStatus = true
+      }
+    }
+  }
+
+  if(!isNaN(competQuantity.value)){         // si l'input n'est pas vide et est un nombre
+    status.competQuantStatus = true
+  }
+
+  locationCheckBox.forEach( (box) => {      // vérifie qu'une box localistion est checké
+    if(box.checked){
+      status.locationBoxStatus = true
+    }
+  })
+
+  if(requiredCheckBox.checked){             // vérifie si la box est checké
+    status.requiredBoxStatus = true
+  }
+
+  let arrayStatus = Object.values(status)         //on transforme l'objet status en tableau
+  let arrayError = Object.values(errorMsg)        //on transforme l'objet errorMsg en tableau
+
+  for (let i=0; i<arrayStatus.length; i++){       // on parcour le tableau des statuts de validation
+    if(arrayStatus[i] == false){                  // si non valide
+      event.preventDefault()                      // on annule l'envoie
+      errorSpan[i].textContent = arrayError[i]    // on insère l'erreur dans errorSpan
+    } else {
+      errorSpan[i].textContent = ""               // si status true on efface l'errorSpan
+    }
+  }
+
 }
